71a2b49376ce03e0be896bb52fae81ff37cb9fb9
Eduardo0409 <100179167+Eduardo0409@users.noreply.github.com>
2023-06-14 14:19:20 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Angular---Firebase
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-rhnw3b-vrpidg)
---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RootComponent } from './root/root.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AuthService } from './auth.service';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyAFCK5HwjbbMqNsibDR3i0FAw3bIOLEWSg",
+      authDomain: "angularlist-36a5c.firebaseapp.com",
+      databaseURL: "https://angularlist-36a5c-default-rtdb.firebaseio.com",
+      projectId: "angularlist-36a5c",
+      storageBucket: "angularlist-36a5c.appspot.com",
+      messagingSenderId: "596198204821",
+      appId: "1:596198204821:web:43639963e28877f31f029c",
+      measurementId: "G-C2VMZK0Y0K"
+    }),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule
+  ],
+  declarations: [RootComponent],
+  bootstrap: [RootComponent],
+  providers: [AuthService]
+})
+export class AppModule {}
